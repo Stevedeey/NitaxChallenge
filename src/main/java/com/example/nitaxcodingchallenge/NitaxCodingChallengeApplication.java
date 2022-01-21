4045e1b8f66dca127565064f1e2c071ef5192ccf
stevedeey <stvolutosin69@gmail.com>
2022-01-21 11:20:38 +0100

    sets project boilerplate

---
+++ src/main/java/com/example/nitaxcodingchallenge/NitaxCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.nitaxcodingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitaxCodingChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NitaxCodingChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nitaxcodingchallenge/NitaxCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nitaxcodingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitaxCodingChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
