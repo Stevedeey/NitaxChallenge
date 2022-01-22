9f08d90643098a54c4b94c7ddfd637339c95cca6
stevedeey <stvolutosin69@gmail.com>
2022-01-22 11:45:45 +0100

    creates bean config

--- src/main/java/com/example/nitaxcodingchallenge/configuration/app/BeansConfig.java
+++ src/main/java/com/example/nitaxcodingchallenge/configuration/app/BeansConfig.java
@@ -1,14 +1,2 @@
-package com.example.nitaxcodingchallenge.configuration.app;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-@Configuration
-public class BeansConfig {
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+package com.example.nitaxcodingchallenge.configuration.app;public class BeansConfig {
 }
